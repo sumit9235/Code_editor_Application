8a2873e8cf179adf5b4b0cf42c62972f65dc4856
Sumit Kumar Yadav <sy7985494155@gmail.com>
2023-09-04 10:17:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require("express")
-const cors=require('cors')
-const { codeRouter } = require('./routes/code.route')
-const app=express()
-app.use(cors())
-app.use(express.json())
+const express = require('express');
+const cors=require('cors');
+const { chatRouter } = require('./routes/chat.route');
+require("dotenv").config()
+const app=express();
+app.use(cors());
+app.use(express.json());
 app.get("/",(req,res)=>{
-    res.send("Hello World")
+ res.send("Hello World")
 })
-app.use("/code",codeRouter)
-app.listen(4500,()=>{
-    console.log("Connected to server")
+app.use("/test",chatRouter)
+
+
+app.listen(process.env.PORT,()=>{
+    console.log("Conected to server")
 })
