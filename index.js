f2fbf3585ff1e209e22338a391709605501a297d
Sumit Kumar Yadav <sy7985494155@gmail.com>
2023-09-04 10:21:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors=require('cors');
-const { chatRouter } = require('./routes/chat.route');
+const { chatRouter } = require('./routes/code.route');
 require("dotenv").config()
 const app=express();
 app.use(cors());
